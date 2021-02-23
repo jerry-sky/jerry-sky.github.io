1d50f7382e34e879b6fdea772ec8681f22675322
jerry-sky <jwroczynski@gmail.com>
2021-02-23 16:14:30 +0100

    build-script: future-proofed the stylesheet path so documents nested inside directories still can use the same stylesheet

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ cp -r assets dist/
 cp template/style.css dist/
 
 # prepare the renderer
-renderer="$exe --template template/document-template.html --from markdown-blank_before_header-implicit_figures+lists_without_preceding_blankline+gfm_auto_identifiers --to html --standalone --css style.css -H template/head.html"
+renderer="$exe --template template/document-template.html --from markdown-blank_before_header-implicit_figures+lists_without_preceding_blankline+gfm_auto_identifiers --to html --standalone --css /style.css -H template/head.html"
 
 # render the main readme file
 $renderer readme.md --output dist/index.html
