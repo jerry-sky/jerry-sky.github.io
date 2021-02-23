26b3d1e36056bb32ac65ff79c58260e84350b209
jerry-sky <jwroczynski@gmail.com>
2021-02-23 16:11:25 +0100

    workflows: build: simplfied the Pandoc download script

--- .github/workflows/download-pandoc.sh
+++ .github/workflows/download-pandoc.sh
@@ -10,21 +10,3 @@ tar -x 'pandoc-2.11.4/bin/pandoc' --strip-components=2 -f "$archive"
 
 # remove the archive
 rm "$archive"
-
-# get the current directory
-cur_dir="${BASH_SOURCE%/*}"
-
-# get the target directory
-target="$1"
-
-if [ -z "$target" ]; then
-    # default value is the current directory
-    target="."
-fi
-
-# strip the forward slash at the end if present
-target="${target%/}"
-
-# move the extracted executable to the target directory
-mv "$cur_dir/pandoc" "$target"
-
