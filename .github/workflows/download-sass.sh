047107309f51c96cad46ccfbbf164d9596dae01c
jerry-sky <jwroczynski@gmail.com>
2021-02-23 20:09:55 +0100

    added Sass (Dart) download script

---
+++ .github/workflows/download-sass.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+archive="sass.tar.gz"
+
+# download the archive containing the executable
+curl -L --output "$archive" 'https://github.com/sass/dart-sass/releases/download/1.32.8/dart-sass-1.32.8-linux-x64.tar.gz'
+
+# unpack the archive
+tar -x 'dart-sass/sass' --strip-components=1 -f "$archive"
+
+# remove the archive
+rm "$archive"
+
