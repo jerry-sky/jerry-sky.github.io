5f3aea10492de3d80f081137a0ac4a4a83e8abc8
jerry-sky <jwroczynski@gmail.com>
2021-02-24 15:34:28 +0100

    added an animated background

--- build.sh
+++ build.sh
@@ -13,10 +13,26 @@ fi
 # prepare the output directory
 mkdir -p dist
 cp -r assets dist/
-cp template/style.css dist/
+
+# render the css
+./sass template/style.scss > dist/style.css
 
 # prepare the renderer
 renderer="$exe --template template/document-template.html --from markdown-blank_before_header-implicit_figures+lists_without_preceding_blankline+gfm_auto_identifiers --to html --standalone --css /style.css -H template/head.html"
 
+# prepare the background file
+background="/tmp/background.html"
+# how many particles to generate
+particle_quantity=25
+# wipe the file clean
+printf '' > "$background"
+for j in one two; do
+    for i in slow medium fast; do
+        printf '<div class="background '$i" "$j'">' >> "$background"
+        yes '<div class="background-particle"></div>' | head -n "$particle_quantity" >> "$background"
+        printf '</div>\n' >> "$background"
+    done
+done
+
 # render the main readme file
-$renderer readme.md --output dist/index.html
+$renderer readme.md --output dist/index.html --include-before-body="$background"
